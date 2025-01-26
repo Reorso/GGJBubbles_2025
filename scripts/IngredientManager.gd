028c6fb8437d00bae6d787852e59674087c86814
NamarienB <ieuanblion@gmail.com>
2025-01-26 18:57:05 +0000

    added cauldron script first draft
    added cauldron script to consume and track ingredient inventory

--- scripts/IngredientManager.gd
+++ scripts/IngredientManager.gd
@@ -6,10 +6,12 @@ const PLANT_TEXTURE_1 : String = ("res://assets/ingredient1.tres")
 const PLANT_TEXTURE_2 : String = ("res://assets/ingredient2.tres")
 const PLANT_TEXTURE_3 : String = ("res://assets/ingredient3.tres")
 
-const PLANT_ASSET : String = ("res://scenes/plant_1.tscn")
+const PLANT_ASSET : String = ("res://scenes/ingredient.tscn")
 
 var plantAsset = load(PLANT_ASSET)
 var weightings : Dictionary
+
+var activeIngredients : Dictionary
 
 func _ready() -> void:
 	textures.append(load(PLANT_TEXTURE_1))
@@ -26,13 +28,18 @@ func _ready() -> void:
 
 func spawnIngredient()-> void:
 	var texture = pickTexture()
-	print(texture)
+	#print(texture)
 	
 	var p1 = plantAsset.instantiate()
-	p1.position = Vector2(randf_range(-134.0, 134.0), randf_range(36.0, 44.0))
+	p1.position = Vector2(randf_range(-130.0, 72), randf_range(-46, 36.0))
 	p1.get_node("Plant1").texture = texture
+	p1.set_meta("ingredient",texture.get_meta("Type"))
+	#print(texture.get_meta("Type"))
 
 	add_child(p1)
+	activeIngredients[p1.name] = texture.get_meta("Type")
+	p1.register(self) #give it reference to this object so it can tell us when deleted
+	print("activeIngredients after register:", activeIngredients)
 
 func pickTexture()-> AtlasTexture:
 	var total : int
@@ -47,15 +54,16 @@ func pickTexture()-> AtlasTexture:
 	for key in weightings:		
 		weightBoundaries[key] = weightings[key] + runningTotal 
 		runningTotal += weightBoundaries[key]
-	print(str("boundaries=",weightBoundaries))
-	print(str("roll=", roll))
+	#print(str("boundaries=",weightBoundaries))
+	#print(str("roll=", roll))
 
 	for key in weightBoundaries:
 		if roll <= weightBoundaries[key]:
 			return key
 			
-	return	null;	
+	return	null;
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func unregister(ref: Object):
+	print("unregistering '", ref.name, "'")
+	activeIngredients.erase(ref.name)
+	print("activeIngredients after unregister:", activeIngredients)
--- scripts/character_body_2d.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-
-var velocity2 = Vector2(50,50)
-
-func _process(delta: float) -> void:
-	var collision_info = move_and_collide(velocity2 * delta)
-	if collision_info:
-		velocity2 = velocity2.bounce(collision_info.get_normal())
